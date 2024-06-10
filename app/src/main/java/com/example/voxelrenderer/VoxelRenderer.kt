2afdc3ec1e6f6c4424aae096b7f17ccce2b6295d
AlessandroCanossa <alessandro.canossa.99@gmail.com>
2024-06-10 22:09:59 +0200

    add instanced model matrices
    Signed-off-by: AlessandroCanossa <alessandro.canossa.99@gmail.com>

--- app/src/main/java/com/example/voxelrenderer/VoxelRenderer.kt
+++ app/src/main/java/com/example/voxelrenderer/VoxelRenderer.kt
@@ -12,7 +12,7 @@ class VoxelRenderer : BasicRenderer() {
     private var vao = intArrayOf(0)
     private var shaderHandle = 0
 
-    private var MVPLoc: Int = 0
+    private var VPLoc: Int = 0
 
     private val viewM = FloatArray(16)
     private val modelM = FloatArray(16)
@@ -21,6 +21,7 @@ class VoxelRenderer : BasicRenderer() {
     private val temp = FloatArray(16)
 
     private var angle: Float = 0.0f
+
     init {
         Matrix.setIdentityM(viewM, 0)
         Matrix.setIdentityM(modelM, 0)
@@ -31,16 +32,19 @@ class VoxelRenderer : BasicRenderer() {
     override fun onSurfaceCreated(gl10: GL10?, config: EGLConfig?) {
         super.onSurfaceCreated(gl10, config)
 
-          val vertexShader = """
+        val vertexShader = """
             #version 320 es
             layout(location = 1) in vec3 vPos;
             layout(location = 2) in vec3 color;
-            uniform mat4 MVP;
+            layout(location = 3) in mat4 model;
+            
+            uniform mat4 VP;
+            
             out vec3 colorVarying;
             
             void main() {
                 colorVarying = color;
-                gl_Position = MVP* vec4(vPos, 1);
+                gl_Position = VP * model * vec4(vPos, 1);
             }
         """.trimIndent()
 
@@ -79,19 +83,14 @@ class VoxelRenderer : BasicRenderer() {
             3, 2, 6, 3, 6, 7, // bottom face
         )
 
-        //--1--|--2---|
-        //vx,vy,r,g,b
-//         val verticesAndColors = floatArrayOf(
-//            -1f, -1f, 1f, 0f, 0f,
-//             1f, -1f, 0f, 1f, 0f,
-//             1f, 1f, 0f, 0f, 1f,
-//             -1f, 1f, 1f, 0f, 1f
-//        )
-//
-//        val indices = intArrayOf(
-//            0, 1, 2,
-//            3, 2, 0
-//        )
+        val matrices = (0 until 10).map {
+            val matrix1 = FloatArray(16)
+            Matrix.setIdentityM(matrix1, 0)
+            Matrix.translateM(matrix1, 0, 0f, it.toFloat() / 2, -it.toFloat())
+            Matrix.scaleM(matrix1, 0, 0.5f, 0.5f, 0.5f)
+            matrix1
+        }.flatMap { it.toList() }.toFloatArray()
+
 
         val vertexData = ByteBuffer.allocateDirect(cubeVertices.size * Float.SIZE_BYTES).run {
             order(ByteOrder.nativeOrder())
@@ -109,9 +108,21 @@ class VoxelRenderer : BasicRenderer() {
             }
         }
 
-        val vbo = IntArray(2)
+        val matricesData = ByteBuffer.allocateDirect(matrices.size * Float.SIZE_BYTES).run {
+            order(ByteOrder.nativeOrder())
+            asFloatBuffer().apply {
+                put(matrices)
+                position(0)
+            }
+        }
 
-        gl.glGenBuffers(2, vbo, 0)
+        val vbo = IntArray(1)
+        val ebo = IntArray(1)
+        val modelVBO = IntArray(1)
+
+        gl.glGenBuffers(1, vbo, 0)
+        gl.glGenBuffers(1, ebo, 0)
+        gl.glGenBuffers(1, modelVBO, 0)
         gl.glGenVertexArrays(1, vao, 0)
 
         gl.glBindVertexArray(vao[0])
@@ -129,7 +140,7 @@ class VoxelRenderer : BasicRenderer() {
         gl.glEnableVertexAttribArray(1)
         gl.glEnableVertexAttribArray(2)
 
-        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, vbo[1])
+        gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ebo[0])
         gl.glBufferData(
             gl.GL_ELEMENT_ARRAY_BUFFER,
             Int.SIZE_BYTES * indexData.capacity(),
@@ -137,13 +148,53 @@ class VoxelRenderer : BasicRenderer() {
             gl.GL_STATIC_DRAW
         )
 
+        // instanced data (model matrices)
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, modelVBO[0])
+        gl.glBufferData(
+            gl.GL_ARRAY_BUFFER,
+            Float.SIZE_BYTES * matricesData.capacity(),
+            matricesData,
+            gl.GL_STATIC_DRAW
+        )
+        gl.glVertexAttribPointer(3, 4, gl.GL_FLOAT, false, 16 * Float.SIZE_BYTES, 0)
+        gl.glEnableVertexAttribArray(3)
+        gl.glVertexAttribPointer(
+            4,
+            4,
+            gl.GL_FLOAT,
+            false,
+            16 * Float.SIZE_BYTES,
+            4 * Float.SIZE_BYTES
+        )
+        gl.glEnableVertexAttribArray(4)
+        gl.glVertexAttribPointer(
+            5,
+            4,
+            gl.GL_FLOAT,
+            false,
+            16 * Float.SIZE_BYTES,
+            8 * Float.SIZE_BYTES
+        )
+        gl.glEnableVertexAttribArray(5)
+        gl.glVertexAttribPointer(
+            6,
+            4,
+            gl.GL_FLOAT,
+            false,
+            16 * Float.SIZE_BYTES,
+            12 * Float.SIZE_BYTES
+        )
+        gl.glEnableVertexAttribArray(6)
+        gl.glVertexAttribDivisor(3, 1)
+        gl.glVertexAttribDivisor(4, 1)
+        gl.glVertexAttribDivisor(5, 1)
+        gl.glVertexAttribDivisor(6, 1)
+        gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
+
         gl.glBindVertexArray(0)
 
-        MVPLoc = gl.glGetUniformLocation(shaderHandle, "MVP");
+        VPLoc = gl.glGetUniformLocation(shaderHandle, "VP");
 
-//        gl.glEnable(gl.GL_CULL_FACE)
-//        gl.glCullFace(gl.GL_BACK)
-//        gl.glFrontFace(gl.GL_CW)
         gl.glEnable(gl.GL_DEPTH_TEST)
     }
 
@@ -166,40 +217,17 @@ class VoxelRenderer : BasicRenderer() {
         gl.glClear(gl.GL_COLOR_BUFFER_BIT or gl.GL_DEPTH_BUFFER_BIT)
 
         angle += 0.5f
-        Matrix.setIdentityM(modelM, 0)
-        Matrix.translateM(modelM, 0, 0f, 0f, -1f)
-        Matrix.scaleM(modelM, 0, 0.5f, 0.5f, 0.5f)
-        Matrix.rotateM(modelM, 0, angle, 0f, 1f, 0f)
+
+        val translation = FloatArray(16)
 
         Matrix.multiplyMM(temp, 0, projM, 0, viewM, 0)
-        Matrix.multiplyMM(MVP, 0, temp, 0, modelM, 0)
 
         gl.glUseProgram(shaderHandle)
         gl.glBindVertexArray(vao[0])
-//        gl.glUniform3fv(colorHandle, 1, floatArrayOf(1.0f, 1.0f, 1.0f), 0)
 
-        gl.glUniformMatrix4fv(MVPLoc, 1, false, MVP, 0)
-        gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_INT, 0)
+        gl.glUniformMatrix4fv(VPLoc, 1, false, temp, 0)
 
-        Matrix.setIdentityM(modelM, 0)
-        Matrix.translateM(modelM, 0, 0f, -1f, -1f)
-        Matrix.scaleM(modelM, 0, 0.5f, 0.5f, 0.5f)
-        Matrix.rotateM(modelM, 0, angle, 0f, 1f, 0f)
-
-        Matrix.multiplyMM(temp, 0, projM, 0, viewM, 0)
-        Matrix.multiplyMM(MVP, 0, temp, 0, modelM, 0)
-        gl.glUniformMatrix4fv(MVPLoc, 1, false, MVP, 0)
-        gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_INT, 0)
-
-        Matrix.setIdentityM(modelM, 0)
-        Matrix.translateM(modelM, 0, 0f, 1f, -1f)
-        Matrix.scaleM(modelM, 0, 0.5f, 0.5f, 0.5f)
-        Matrix.rotateM(modelM, 0, angle, 0f, 1f, 0f)
-
-        Matrix.multiplyMM(temp, 0, projM, 0, viewM, 0)
-        Matrix.multiplyMM(MVP, 0, temp, 0, modelM, 0)
-        gl.glUniformMatrix4fv(MVPLoc, 1, false, MVP, 0)
-        gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_INT, 0)
+        gl.glDrawElementsInstanced(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_INT, 0, 10)
 
         gl.glBindVertexArray(0)
         gl.glUseProgram(0)
