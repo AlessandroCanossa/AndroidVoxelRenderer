75e9249042b3e73eaf8312bd7b1eb360a9fb17c8
AlessandroCanossa <alessandro.canossa.99@gmail.com>
2024-06-11 20:09:21 +0200

    Update shader class
    Signed-off-by: AlessandroCanossa <alessandro.canossa.99@gmail.com>

--- app/src/main/java/com/example/voxelrenderer/utils/Shader.kt
+++ app/src/main/java/com/example/voxelrenderer/utils/Shader.kt
@@ -1,4 +1,23 @@
 package com.example.voxelrenderer.utils
 
-class Shader {
+import android.opengl.GLES32
+import android.util.Log
+import java.io.InputStream
+
+class Shader(isV: InputStream, isF: InputStream) {
+    private var shaderHandle: Int = 0
+
+    init {
+        shaderHandle = ShaderCompiler.createProgram(isV, isF)
+    }
+
+    fun use(body: () -> Unit) {
+        GLES32.glUseProgram(shaderHandle)
+        body()
+        GLES32.glUseProgram(0)
+    }
+
+    fun getUniformLocation(name: String): Int {
+        return GLES32.glGetUniformLocation(shaderHandle, name)
+    }
 }
